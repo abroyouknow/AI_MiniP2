6ab0e36cef909c24ccc348f2c5f95f9b00a878bd
Cristian Milatinov <milatinovcristian@gmail.com>
2021-11-10 16:23:09 -0500

    add win indices generation + win detection

--- src/main.py
+++ src/main.py
@@ -43,6 +43,50 @@ class Game:
         # Player X always plays first
         self.player_turn = 'X'
 
+        # Generate winning combination indices
+        self.win_indices = []
+
+        # Horizontal / Vertical
+        for i in range(self.n):
+            win_v = []
+            win_h = []
+            for j in range(self.n):
+                win_v.append((i, j))
+                win_h.append((j, i))
+            self.win_indices.append(win_h)
+            self.win_indices.append(win_v)
+
+        # Diagonal
+        for i in range(self.s - 1, self.n):
+            #
+            offset_top_right = (1, -1)
+            offset_top_left = (-1, -1)
+            win_bl_tr1 = []
+            win_bl_tr2 = []
+            win_br_tl1 = []
+            win_br_tl2 = []
+            for j in range(i + 1):
+                # Bottom Left to Top Right
+                diag_bl_tr1 = (offset_top_right[0] * j, offset_top_right[1] * j + i)
+                diag_bl_tr2 = (offset_top_right[0] * j + self.n - 1 - i, offset_top_right[1] * j + self.n - 1)
+                # Bottom Right to Top Left
+                diag_br_tl1 = (offset_top_left[0] * j + self.n - 1, offset_top_left[1] * j + i)
+                diag_br_tl2 = (offset_top_left[0] * j + i, offset_top_left[1] * j + self.n - 1)
+                # Add points to lists
+                win_bl_tr1.append(diag_bl_tr1)
+                win_br_tl1.append(diag_br_tl1)
+                win_bl_tr2.append(diag_bl_tr2)
+                win_br_tl2.append(diag_br_tl2)
+
+            # Append each diagonal
+            self.win_indices.append(win_bl_tr1)
+            self.win_indices.append(win_br_tl1)
+
+            # Mirror diagonal if not largest
+            if i + 1 != self.n:
+                self.win_indices.append(win_bl_tr2)
+                self.win_indices.append(win_br_tl2)
+
     def draw_board(self):
         print()
         for y in range(0, self.n):
@@ -60,31 +104,32 @@ class Game:
             return True
 
     def is_end(self):
-        # limit for win
-        limit_for_win = self.n - self.s + 1
+        # # limit for win
+        # limit_for_win = self.n - self.s + 1
+        #
+        # # Vertical win
+        # for column in range(self.n):
+        #     for c in range(limit_for_win):
+        #         if self.current_state[column][c] != '.' and self.current_state[column][c] != '*':
+        #             score = 1
+        #             for next_c in range(c + 1, self.n):
+        #                 if self.current_state[column][c] != self.current_state[column][next_c]:
+        #                     break
+        #                 score += 1
+        #                 if score == self.s:
+        #                     return self.current_state[column][next_c]
+        # # Horizontal win
+        # for i in range(self.n):
+        #     for j in range(limit_for_win):
+        #         if self.current_state[i][j] != '.' and self.current_state[i][j] != '*':
+        #             score = 1
+        #             for k in range(j + 1, self.n):
+        #                 if self.current_state[i][j] != self.current_state[i][k]:
+        #                     break
+        #                 score += 1
+        #                 if score == self.s:
+        #                     return self.current_state[i][j]
 
-        # Vertical win
-        for column in range(self.n):
-            for c in range(limit_for_win):
-                if self.current_state[column][c] != '.' and self.current_state[column][c] != '*':
-                    score = 1
-                    for next_c in range(c + 1, self.n):
-                        if self.current_state[column][c] != self.current_state[column][next_c]:
-                            break
-                        score += 1
-                        if score == self.s:
-                            return self.current_state[column][next_c]
-        # Horizontal win
-        for i in range(0, self.n):
-            for j in range(0, limit_for_win):
-                if self.current_state[i][j] != '.' and self.current_state[i][j] != '*':
-                    score = 1
-                    for k in range(j + 1, self.n):
-                        if self.current_state[i][j] != self.current_state[i][k]:
-                            break
-                        score += 1
-                        if score == self.s:
-                            return self.current_state[i][j]
         # # Main diagonal win
         # if (self.current_state[0][0] != '.' and
         #         self.current_state[0][0] == self.current_state[1][1] and
@@ -96,11 +141,51 @@ class Game:
         #         self.current_state[0][2] == self.current_state[2][0]):
         #     return self.current_state[0][2]
         # Is whole board full?
+        # for i in range(0, self.n):
+        #     for j in range(0, self.n):
+        #         # There's an empty field, we continue the game
+        #         if self.current_state[i][j] == '.':
+        #             return None
+
+        # Iterate over all possible winning lines
+        for win in self.win_indices:
+            # Keep track of last k symbols in a row (and corresponding player)
+            last_player = None
+            score = 0
+            for i in range(len(win)):
+                # Get the symbol at coordinates
+                coords = win[i]
+                symbol = self.current_state[coords[0]][coords[1]]
+
+                # Symbol is the same as the last one, increment score
+                if symbol == last_player:
+                    score += 1
+
+                # Symbol belongs to a different player than last symbol
+                elif symbol != '.' and symbol != '*':
+
+                    # Attempt match S players symbols in a row to win
+                    last_player = symbol
+                    score = 1
+
+                # Symbol is empty or block
+                else:
+                    last_player = None
+
+                    # Not enough symbols left to make S in a row
+                    if len(win) - i < self.s:
+                        break
+
+                # Player wins
+                if score >= self.s:
+                    return last_player
+
         for i in range(0, self.n):
             for j in range(0, self.n):
                 # There's an empty field, we continue the game
                 if self.current_state[i][j] == '.':
                     return None
+
         # It's a tie!
         return '.'
 
@@ -247,22 +332,22 @@ class Game:
             if self.check_end():
                 return
             start = time.time()
-            # if algo == self.MINIMAX:
-            #     if self.player_turn == 'X':
-            #         (_, x, y) = self.minimax(max=False)
-            #     else:
-            #         (_, x, y) = self.minimax(max=True)
-            # else:  # algo == self.ALPHABETA
-            #     if self.player_turn == 'X':
-            #         (m, x, y) = self.alphabeta(max=False)
-            #     else:
-            #         (m, x, y) = self.alphabeta(max=True)
-            # end = time.time()4
+            if algo == self.MINIMAX:
+                if self.player_turn == 'X':
+                    (_, x, y) = self.minimax(max=False)
+                else:
+                    (_, x, y) = self.minimax(max=True)
+            else:  # algo == self.ALPHABETA
+                if self.player_turn == 'X':
+                    (m, x, y) = self.alphabeta(max=False)
+                else:
+                    (m, x, y) = self.alphabeta(max=True)
+            end = time.time()
             if (self.player_turn == 'X' and player_x == self.HUMAN) or (
                     self.player_turn == 'O' and player_o == self.HUMAN):
-                # if self.recommend:
-                #     print(F'Evaluation time: {round(end - start, 7)}s')
-                #     print(F'Recommended move: x = {x}, y = {y}')
+                if self.recommend:
+                    print(F'Evaluation time: {round(end - start, 7)}s')
+                    print(F'Recommended move: x = {x}, y = {y}')
                 (x, y) = self.input_move()
             if (self.player_turn == 'X' and player_x == self.AI) or (self.player_turn == 'O' and player_o == self.AI):
                 print(F'Evaluation time: {round(end - start, 7)}s')
